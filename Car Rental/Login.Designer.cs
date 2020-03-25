d7c1c06a10baf00d23b75115d9005a611e39fbd1
CreXcel <58618179+CreXcel@users.noreply.github.com>
2020-03-24 19:41:31 -0600

    Car Rental Project Login
    This is the project/solution I will now be working on and updating, the others were a test to connect to SQL.
    In order to make the login have full functionality to work, please change line 56 in Login.cs to your local connection string.

---
+++ Car Rental/Login.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace SQLTest
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.LogoLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usrTBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passTBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(170, 427);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.LogoLabel);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(170, 100);
+            this.panel3.TabIndex = 3;
+            // 
+            // LogoLabel
+            // 
+            this.LogoLabel.AutoSize = true;
+            this.LogoLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoLabel.ForeColor = System.Drawing.Color.White;
+            this.LogoLabel.Location = new System.Drawing.Point(12, 42);
+            this.LogoLabel.Name = "LogoLabel";
+            this.LogoLabel.Size = new System.Drawing.Size(157, 22);
+            this.LogoLabel.TabIndex = 3;
+            this.LogoLabel.Text = "Logo Goes Here";
+            this.LogoLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.exitButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(170, 326);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(934, 101);
+            this.panel2.TabIndex = 2;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitButton.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(588, 3);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(184, 86);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.UsernameText);
+            // 
+            // usrTBox
+            // 
+            this.usrTBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.usrTBox.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.usrTBox.Location = new System.Drawing.Point(417, 69);
+            this.usrTBox.Multiline = true;
+            this.usrTBox.Name = "usrTBox";
+            this.usrTBox.Size = new System.Drawing.Size(269, 42);
+            this.usrTBox.TabIndex = 5;
+            this.usrTBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.usrTBox_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.PasswordText);
+            // 
+            // passTBox
+            // 
+            this.passTBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.passTBox.Location = new System.Drawing.Point(417, 167);
+            this.passTBox.Multiline = true;
+            this.passTBox.Name = "passTBox";
+            this.passTBox.PasswordChar = '•';
+            this.passTBox.Size = new System.Drawing.Size(269, 45);
+            this.passTBox.TabIndex = 7;
+            this.passTBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.passTBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passTBox_KeyDown);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.LoginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoginButton.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(758, 60);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(175, 69);
+            this.LoginButton.TabIndex = 8;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.button1_Click_1);
+            this.LoginButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LoginButton_KeyDown);
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.LoginButton;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1104, 427);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.passTBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.usrTBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Login";
+            this.Text = "Login to Car Rental";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            //((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label LogoLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usrTBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passTBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
