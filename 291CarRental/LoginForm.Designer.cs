b1a2b59e2af14d0ed196652ef14a8efcd6c022b4
CreXcel <58618179+CreXcel@users.noreply.github.com>
2020-03-27 12:42:59 -0600

    More Efficient and Secure Files
    Use this project as a base to add upon your files. Comments are in my project to show you where to put it
    Also to show your file, don't use applicationForm.Show, use applicationForm.ShowDialog() as using .Show() opens an entirely separate thread, thus adding more work to CPU. ShowDialog() is less taxing, and more efficient.

---
+++ 291CarRental/LoginForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace _291CarRental
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.registerLabel = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(30, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(183)))), ((int)(((byte)(19)))));
+            this.panel1.Controls.Add(this.LoginLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(942, 83);
+            this.panel1.TabIndex = 1;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Century Schoolbook", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.passwordLabel.Location = new System.Drawing.Point(41, 215);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(279, 55);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(350, 118);
+            this.username.Multiline = true;
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(359, 59);
+            this.username.TabIndex = 3;
+            this.username.Text = "Enter username";
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(350, 215);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.PasswordChar = '•';
+            this.password.Size = new System.Drawing.Size(359, 59);
+            this.password.TabIndex = 4;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.Teal;
+            this.LoginButton.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(750, 118);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(171, 68);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Brown;
+            this.ExitButton.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(750, 212);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(171, 68);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // registerLabel
+            // 
+            this.registerLabel.AutoSize = true;
+            this.registerLabel.Font = new System.Drawing.Font("Century Schoolbook", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.registerLabel.Location = new System.Drawing.Point(87, 341);
+            this.registerLabel.Name = "registerLabel";
+            this.registerLabel.Size = new System.Drawing.Size(444, 30);
+            this.registerLabel.TabIndex = 7;
+            this.registerLabel.Text = "Don\'t have an account? Click here:";
+            // 
+            // registerButton
+            // 
+            this.registerButton.BackColor = System.Drawing.Color.Silver;
+            this.registerButton.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerButton.Location = new System.Drawing.Point(537, 328);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(347, 53);
+            this.registerButton.TabIndex = 8;
+            this.registerButton.Text = "Create an account";
+            this.registerButton.UseVisualStyleBackColor = false;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginLabel.Location = new System.Drawing.Point(343, 19);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(257, 42);
+            this.LoginLabel.TabIndex = 0;
+            this.LoginLabel.Text = "USER LOGIN";
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(942, 407);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.registerLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "loginForm";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label registerLabel;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Label LoginLabel;
+    }
+}
+
---
+++ 291CarRental/RegisterClient.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace _291CarRental
+{
+    public partial class RegisterClient : Form
+    {
+        public database datab;
+        public RegisterClient(database temp)
+        {
+            InitializeComponent();
+            datab = temp;
+        }
+
+        private void Phone3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            checkInput();
+            Guid ClientID = Guid.NewGuid();
+            string Phone = Phone1.Text + Phone2.Text + Phone3.Text;
+
+            try
+            {
+                /* 
+                 * To account for SQL injection, I'm attempting to use parameter values instead to account for apostrophes
+                 */
+                
+                 string cmd = "Insert into dbo.Client (CustomerID, [Total Year Rent], [First Name], [Last Name], Address, CreditCardNum, Phone) values (@Client, @Rent, @First, @Last, @Address, @Credit, @Phone)";
+                 datab.myCommand.CommandText = cmd;
+                 //datab.myCommand.Parameters.AddWithValue("Client", NEWID());
+                 datab.myCommand.Parameters.AddWithValue("Client", ClientID);
+                 datab.myCommand.Parameters.AddWithValue("Rent", DBNull.Value);
+                 datab.myCommand.Parameters.AddWithValue("First", FirstName.Text);
+                 datab.myCommand.Parameters.AddWithValue("Last", LastName.Text);
+                 datab.myCommand.Parameters.AddWithValue("Address", Address.Text);
+                 datab.myCommand.Parameters.AddWithValue("Credit", DBNull.Value);
+                 datab.myCommand.Parameters.AddWithValue("Phone", Phone);
+
+                 //MessageBox.Show(datab.myCommand.CommandText);
+
+                 datab.myCommand.ExecuteNonQuery();
+                 MessageBox.Show("You have reigistered!", "FINALIZE");
+               
+
+            }
+            
+            catch
+            {
+                HandleError();
+            }
+            
+
+        }
+
+        private void Phone1_TextChanged(object sender, EventArgs e)
+        {
+            if (Phone1.TextLength == 3)
+            {
+                Phone2.Focus();
+            }
+        }
+
+        private void Phone2_TextChanged(object sender, EventArgs e)
+        {
+            if (Phone2.TextLength == 3)
+            {
+                Phone3.Focus();
+            }
+        }
+
+        private void RegisterClient_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void reset()
+        {
+            RegisterClient registerClient = new RegisterClient(datab);
+            this.Dispose(false);
+            this.Close();
+            registerClient.ShowDialog();
+        }
+        public void HandleError()
+        {
+            MessageBox.Show("ERROR: Something seems to have failed. Please check if your information is correct, or if all information needed is inputted.", "ERROR");
+            reset();
+
+        }
+        public void checkInput()
+        {
+            if (FirstName.Text == "First Name")
+                HandleError();
+            if (LastName.Text == "Last Name")
+                HandleError();
+            if (Address.Text == "Address")
+                HandleError();
+        }
+    }
+}
