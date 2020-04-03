e6c7b7373fc611bc1dcb350974d012eafef29433
Ross Beaudin <beaudinr@mymacewan.ca>
2020-04-03 11:50:41 -0600

    update to final version

--- 291CarRental/Properties/Settings.Designer.cs
+++ 291CarRental/Properties/Settings.Designer.cs
@@ -37,8 +37,7 @@ namespace _291CarRental.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-9JM8HEJ;Initial Catalog=291GroupProject;Integrated Security=T" +
-            "rue")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=X-MK1;Initial Catalog=291GroupProject;Integrated Security=True")]
         public string _291GroupProjectDataSet {
             get {
                 return ((string)(this["_291GroupProjectDataSet"]));
@@ -58,8 +57,7 @@ namespace _291CarRental.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-9JM8HEJ;Initial Catalog=291GroupProject;Integrated Security=T" +
-            "rue")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=X-MK1;Initial Catalog=291GroupProject;Integrated Security=True")]
         public string _291GroupProjectConnectionString1 {
             get {
                 return ((string)(this["_291GroupProjectConnectionString1"]));
